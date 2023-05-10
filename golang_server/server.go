37a93499dde2e6474c5651ddaaf4669c6a74ecb8
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-05-10 11:19:11 +0900

    add cat-lang

--- golang_server/server.go
+++ golang_server/server.go
@@ -3,9 +3,11 @@ package main
 import (
 	"fmt"
 	"net"
+	"strings"
 )
 
 func main() {
+	fmt.Println("-----Go Server-----")
 	// ポート8000で待ち受ける
 	listener, err := net.Listen("tcp", ":8000")
 	if err != nil {
@@ -24,36 +26,64 @@ func main() {
 		fmt.Println("Client connected.")
 
 		// クライアントとの接続を別々のgoroutineで処理
-		go handleConnection(conn)
+		go handleClient(conn)
 	}
 }
 
-func handleConnection(conn net.Conn) {
+func handleClient(conn net.Conn) {
 	// 関数が戻る時にコネクションを閉じる
 	defer conn.Close()
 
 	// データを受け取るためのバッファ
 	buf := make([]byte, 1024)
 
-	// コネクションが閉じるまでデータを読み取る
-	for {
-		n, err := conn.Read(buf)
-		if err != nil {
-			fmt.Println("Connection closed.")
-			return
-		}
-
-		// 受信したバイト列を文字列に変換して表示
-		msg := string(buf[:n])
-		fmt.Printf("Received message: %s\n", msg)
-
-		// クライアントにメッセージを返す
-		resp := "Message received"
-		_, err = conn.Write([]byte(resp))
-		if err != nil {
-			panic(err)
-		}
-		fmt.Printf("Sent message: %s\n", resp)
+	// データを読み取る
+	n, err := conn.Read(buf)
+	if err != nil {
+		fmt.Println("Error reading data:", err.Error())
+		return
 	}
+
+	// 受信したバイト列を文字列に変換して表示
+	msg := string(buf[:n])
+	catMessage := convertToCatLanguage(msg)
+
+
+	_, err = conn.Write([]byte(catMessage))
+	if err != nil{
+		fmt.Println("Error sending data:", err.Error())
+		return
+	}
+
+	fmt.Println("Message sent to client:", catMessage)
 }
 
+func convertToCatLanguage(message string) string {
+	// 猫語に変換するロジックを実装する
+
+	var builder strings.Builder
+	for _, char := range message {
+		// 文字コードをバイトスライスに変換
+		bytes := []byte(string(char))
+
+		// バイトスライスの各バイトを2ビットずつ取り出して猫語に変換
+		for _, b := range bytes {
+			// バイトの上位6ビットを無視
+			b = b & 0x03
+
+			// 2ビットごとに変換
+			switch b {
+			case 0x00:
+				builder.WriteString("ニャン")
+			case 0x01:
+				builder.WriteString("ミャン")
+			case 0x02:
+				builder.WriteString("ミャオン")
+			case 0x03:
+				builder.WriteString("ニャーン")
+			}
+		}
+	}
+
+	return builder.String()
+}
--- rust_client/client/src/main.rs
+++ rust_client/client/src/main.rs
@@ -1,11 +1,17 @@
+use std::io;
 use std::io::prelude::*;
 use std::net::TcpStream;
 
 fn main() -> std::io::Result<()> {
+    println!("-----Rust Client-----");
+
     let mut stream = TcpStream::connect("127.0.0.1:8000")?;
 
     // メッセージ文字列をバイト配列に変換して、サーバーに送信
-    let msg = "Hello, server!";
+    println!("Input message:");
+    let mut msg = String::new();
+    io::stdin().read_line(&mut msg)
+    	       .expect("Failed to read");
     stream.write(msg.as_bytes())?;
     println!("Sent message: {}", msg);
 
@@ -21,7 +27,7 @@ fn main() -> std::io::Result<()> {
 
         // 受信したバイト列を文字列に変換して表示
         let resp = String::from_utf8_lossy(&buf[..n]);
-        println!("Received message: {}", resp);
+        println!("Received server message: {}", resp);
     }
 
     Ok(())
