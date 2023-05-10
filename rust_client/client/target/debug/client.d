567161b93f104eb36850bb4e9d0fffc3b567e2f9
takato256 <c1139952@planet.kanazawa-it.ac.jp>
2023-05-10 11:22:40 +0900

    delete .gitignore

---
+++ rust_client/client/target/debug/client.d
@@ -0,0 +1,1 @@
+/home/ubuntu/multi-socket-programming/a/rust_client/client/target/debug/client: /home/ubuntu/multi-socket-programming/a/rust_client/client/src/main.rs
---
+++ rust_client/client/target/debug/deps/client-6c2d4de0405b86fb.d
@@ -0,0 +1,5 @@
+/home/ubuntu/multi-socket-programming/a/rust_client/client/target/debug/deps/client-6c2d4de0405b86fb: src/main.rs
+
+/home/ubuntu/multi-socket-programming/a/rust_client/client/target/debug/deps/client-6c2d4de0405b86fb.d: src/main.rs
+
+src/main.rs:
